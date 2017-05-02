decc9452f8f4c360189505f06dcc9a66bfdf1755
David Kalish <davidpkalish@gmail.com>
2017-05-01 21:42:58 -0400

    added face control early workings

--- WinMine.java
+++ WinMine.java
@@ -9,26 +9,81 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+enum Face {
+    OK,
+    NERVOUS,
+    DEAD,
+    WON
+}
+
 class MineButton extends JButton {
     int adj_mines;
     boolean isMine;
-
-    MineButton(int myAdjMines, boolean myIsMine) {
-        super("");
-        this.adj_mines = myAdjMines;
-        this.isMine = myIsMine;
-    }
 
     MineButton(String s, int myAdjMines, boolean myIsMine) {
         super(s);
         this.adj_mines = myAdjMines;
         this.isMine = myIsMine;
     }
+
+    MineButton(int myAdjMines, boolean myIsMine) {
+        this("", myAdjMines, myIsMine);
+    }
+
+    private void click() {
+        super.setContentAreaFilled(false);
+        super.setBorderPainted(true);
+        super.setOpaque(false);
+    }
+}
+
+class FaceButton extends JButton {
+    Face face;
+
+    FaceButton(Face myFace) {
+        this.face = myFace;
+    }
+    FaceButton() {
+        this.face = Face.OK;
+    }
+
+    void showFace(Face myface) {
+        this.face = myface;
+        ImageIcon faceImg;
+        switch (face) {
+            case OK:
+                faceImg = new ImageIcon(
+                    getClass().getResource("img/face-ok.png"));
+                break;
+            case NERVOUS:
+                faceImg = new ImageIcon(
+                    getClass().getResource("img/face-nervous.png"));
+                break;
+            case WON:
+                faceImg = new ImageIcon(
+                    getClass().getResource("img/face-won.png"));
+                break;
+            case DEAD:
+                faceImg = new ImageIcon(
+                    getClass().getResource("img/face-dead.png"));
+                break;
+            default:
+                faceImg = new ImageIcon(
+                    getClass().getResource("img/face-ok.png"));
+                break;
+        }
+        // System.out.println(faceImg);
+        // System.out.println(faceImg.getImageLoadStatus());
+        this.setIcon(faceImg);
+
+    }
 }
 
 class WinMine {
     public static JTextField MINES_LEFT = new JTextField(2);
     public static JTextField TIMER = new JTextField(2);
+    public static FaceButton FACEBUTTON;
+    public static JPanel MINESREMAININGDISPLAY;
 
     public static void main(String[] args) {
         doLayout();
@@ -49,6 +104,7 @@ class WinMine {
         // make the layout.
         // make the upper panels
         JPanel timerDisplay = makeDisplay(MINES_LEFT);
+        makeFaceButton();
         JPanel minesRemainingDisplay = makeDisplay(TIMER);
 
         // put upper panel things into a panel
@@ -56,10 +112,11 @@ class WinMine {
         BorderLayout bordLay = new BorderLayout(0,0);
         upperPanel.setLayout(bordLay);
         upperPanel.add(timerDisplay, bordLay.LINE_START);
+        upperPanel.add(FACEBUTTON, bordLay.CENTER);
         upperPanel.add(minesRemainingDisplay, bordLay.LINE_END);
 
         // make the game board grid
-        JPanel buttonPanel = makeButtonPanel(wmWindow, 8, 8);
+        JPanel buttonPanel = makeButtonGrid(wmWindow, 8, 8);
 
         // build the window
         wmWindow.add(upperPanel, BorderLayout.PAGE_START);
@@ -70,7 +127,13 @@ class WinMine {
         wmWindow.setVisible(true);
     }
 
-    private static JPanel makeButtonPanel(JFrame window, int rows, int cols) {
+    private static void makeFaceButton() {
+        FACEBUTTON = new FaceButton();
+        FACEBUTTON.showFace(Face.OK);
+        FACEBUTTON.setPreferredSize(new Dimension(34, 34));
+    }
+
+    private static JPanel makeButtonGrid(JFrame window, int rows, int cols) {
         JPanel butPan = new JPanel(new GridLayout(rows, cols));
 
         MineButton[][] mineGrid = new MineButton[rows][cols];
@@ -78,12 +141,30 @@ class WinMine {
         // make the buttons
         for (int x = 0; x < rows; x++) {
             for (int y = 0; y < cols; y++) {
-                mineGrid[x][y] = new MineButton(Integer.toString((rows * x) + y), 1, true);
+                mineGrid[x][y] = new MineButton(1, true);
+                // mineGrid[x][y] = new MineButton(Integer.toString((rows * x) + y), 1, true);
                 butPan.add(mineGrid[x][y]);
                 mineGrid[x][y].setPreferredSize(new Dimension(50, 50));
+
+                // set up mouse listeners for button clicks
+                mineGrid[x][y].addMouseListener(new MouseAdapter(){
+                    public void mousePressed(MouseEvent mp) {
+                        mousePressedHandler(mp);
+                    }
+                    public void mouseReleased(MouseEvent mr) {
+                        mouseReleasedHandler(mr);
+                    }
+                });
             }
         }
         return butPan;
+    }
+
+    private static void mousePressedHandler(MouseEvent mp) {
+        FACEBUTTON.showFace(Face.NERVOUS);
+    }
+    private static void mouseReleasedHandler(MouseEvent mr) {
+        FACEBUTTON.showFace(Face.OK);
     }
 
 
