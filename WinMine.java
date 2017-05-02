a8a996c7d989a716f2e2378d804e60161ff2b359
David Kalish <davidpkalish@gmail.com>
2017-05-02 14:57:27 -0400

    adjacent mine numbers works!!

--- WinMine.java
+++ WinMine.java
@@ -10,74 +10,102 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 
-enum Face {
-    OK,
-    NERVOUS,
-    DEAD,
-    WON
-}
+// different looks for the face icon
+enum Face {OK,
+           NERVOUS,
+           DEAD,
+           WON
+          }
 
-enum MOUSE_STATUS {
-    PRESSED,
-    RELEASED,
-    EXITED
-}
+// for keeping the mouseReleased from firing if the mouse has been moved off
+// the tile before release
+enum MOUSE_STATUS {PRESSED,
+                   RELEASED,
+                   EXITED
+                  }
 
 class MineButton extends JButton {
-    private int adj_mines;
+    // Class for the mine button grid, extending JButton. 
+    private int adjMines;
     private boolean isMine;
     private boolean flagged;
     private boolean revealed;
+    private int row;
+    private int col;
 
-    MineButton(String s, int myAdjMines, boolean myIsMine) {
+    MineButton(String s, boolean myIsMine, int myRow, int myCol) {
+        // MineButton constructor
         super(s);
         super.setRolloverEnabled(false);
         super.setFocusPainted(false);
-        this.adj_mines = myAdjMines;
         this.isMine = myIsMine;
         this.flagged = false;
         this.revealed = false;
+        this.row = myRow;
+        this.col = myCol;
     }
 
-    MineButton(int myAdjMines, boolean myIsMine) {
-        this("", myAdjMines, myIsMine);
+    MineButton(boolean myIsMine, int myRow, int myCol) {
+        // MineButton constructor (no string first arg)
+        this("", myIsMine, myRow, myCol);
     }
 
     public void decorateClicked() {
+        // Decorate the tile when it has been clicked (mouseReleased).
+        // Button look goes away
         super.setContentAreaFilled(false);
         super.setBorderPainted(true);
         super.setOpaque(false);
         this.revealed = true;
 
+        // if the tile has a mine, show the mine icon
         ImageIcon tileIcon;
         if (hasMine()) {
             tileIcon = new ImageIcon(getClass().getResource("img/mine.png"));
+            this.setIcon(tileIcon);
         } else {
-            tileIcon = new ImageIcon(getClass().getResource("img/num-2.png"));
+        // if no mine, display number of adjacent mines
+            decorateAdjMineNum();
         }
-        this.setIcon(tileIcon);
+    }
+
+    public void setAdjMines(int n) {
+        // set this tile's number of adjacent mines
+        this.adjMines = n;
+    }
+
+    public int[] getPosition() {
+        // return the {row, column} position of this mine
+        int[] position = {this.row, this.col};
+        return position;
     }
 
     public boolean hasMine() {
+        // return whether or not this tile has a mine 
         return this.isMine;
     }
 
     public boolean getRevealed() {
+        // return whether or not this tile has already been revealed
         return this.revealed;
     }
 
     public void toggleFlag() {
+        // toggle whether this mine has a flag or not
         if (this.flagged) this.flagged = false;
         else this.flagged = true;
 
+        // display/remove the flag icon
         this.decorateFlag();
     }
 
     public boolean getFlagged() {
+        // return whether or not this tile is currently flagged
         return this.flagged;
     }
 
     private void decorateFlag() {
+        // show a flag if this.flagged == true, remove it if its == false
         if (getFlagged()) {
             ImageIcon flagIcon = new ImageIcon(
                 getClass().getResource("img/flag.png"));
@@ -101,72 +129,128 @@ class MineButton extends JButton {
             }
         });
     }
+
+    private void decorateAdjMineNum() {
+        // show the number of mines adjacent to this tile. leave blank if no
+        // adjacent mines.
+        ImageIcon numIcon;
+        int n = this.adjMines;
+        String imgPath;
+
+        // get number image for # of adjacent mines
+        switch (n) {
+            case 1:
+                imgPath = "img/num-1.png";
+                break;
+            case 2:
+                imgPath = "img/num-2.png";
+                break;
+            case 3:
+                imgPath = "img/num-3.png";
+                break;
+            case 4:
+                imgPath = "img/num-4.png";
+                break;
+            case 5:
+                imgPath = "img/num-5.png";
+                break;
+            case 6:
+                imgPath = "img/num-6.png";
+                break;
+            case 7:
+                imgPath = "img/num-7.png";
+                break;
+            case 8:
+                imgPath = "img/num-8.png";
+                break;
+            default:
+                imgPath = null;
+                break;
+        }
+        // if path defined, show number icon
+        if (imgPath != null) {
+            numIcon = new ImageIcon(getClass().getResource(imgPath));
+            this.setIcon(numIcon);
+        // if path null, leave it blank (no adjacent mines)
+        } else this.setIcon(null);
+    }
 }
 
 class FaceButton extends JButton {
+    // Class for the fac button at the top of the window.
     Face face;
 
     FaceButton(Face myFace) {
+        // FaceButton constructor
         this.face = myFace;
         super.setFocusPainted(false);
         showFace(this.face);
     }
     FaceButton() {
+        // FaceButton constructor
         this(Face.OK);
     }
 
     void showFace(Face myface) {
+        // Show one of the four possible faces, depending on the enum Face arg
         this.face = myface;
         ImageIcon faceImg;
+        String imgPath;
+
         switch (face) {
             case OK:
-                faceImg = new ImageIcon(
-                    getClass().getResource("img/face-ok.png"));
+                imgPath = "img/face-ok.png";
                 break;
             case NERVOUS:
-                faceImg = new ImageIcon(
-                    getClass().getResource("img/face-nervous.png"));
+                imgPath = "img/face-nervous.png";
                 break;
             case WON:
-                faceImg = new ImageIcon(
-                    getClass().getResource("img/face-won.png"));
+                imgPath = "img/face-won.png";
                 break;
             case DEAD:
-                faceImg = new ImageIcon(
-                    getClass().getResource("img/face-dead.png"));
+                imgPath = "img/face-dead.png";
                 break;
             default:
-                faceImg = new ImageIcon(
-                    getClass().getResource("img/face-ok.png"));
+                imgPath = "img/face-ok.png";
                 break;
         }
+        faceImg = new ImageIcon(getClass().getResource(imgPath));
         // System.out.println(faceImg);
         // System.out.println(faceImg.getImageLoadStatus());
         this.setIcon(faceImg);
-
     }
 }
 
 class WinMine {
+    // Main class for the game
+
+    // game window
     public static JFrame WM_WINDOW;
+    // text field for remaining mines to be flagged
     public static JTextField MINES_LEFT = new JTextField(2);
-    public static JTextField TIMER = new JTextField(2);
-    public static FaceButton FACEBUTTON;
+    // panel for MINES_LEFT
     public static JPanel MINESREMAININGDISPLAY;
+    // elapsed seconds timer display
+    public static JTextField TIMER = new JTextField(2);
+    // face button
+    public static FaceButton FACEBUTTON;
+    // the main game grid
     public static MineButton[][] MINE_GRID;
 
+    // main function
     public static void main(String[] args) {
         doLayout();
         System.out.println("main()");
     }
 
-    public WinMine() {
-        doLayout();
-        System.out.println("WinMine");
-    }
+    // public WinMine() {
+    //     doLayout();
+    //     System.out.println("WinMine");
+    // }
 
+    // set up the whole game board
     private static void doLayout() {
-        // set up JFrame
+        // set up JFrame window
         WM_WINDOW = new JFrame();
         WM_WINDOW.setResizable(false);
         WM_WINDOW.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -236,7 +320,8 @@ class WinMine {
                                          int numMines) {
         JPanel butPan = new JPanel(new GridLayout(rows, cols));
         MINE_GRID = new MineButton[rows][cols];
-        boolean mine;
+        boolean isMine;
+        int adjMines;
 
         // create and distribute mine tiles
         ArrayList<Integer> mineList = mineLocs(numMines, rows * cols);
@@ -246,10 +331,10 @@ class WinMine {
         for (int x = 0; x < rows; x++) {
             for (int y = 0; y < cols; y++) {
                 // see if this tile should be a mine
-                if (mineList.contains((rows * x) + y)) mine = true;
-                else mine = false;
-                MINE_GRID[x][y] = new MineButton(1, mine);
-                // MINE_GRID[x][y] = new MineButton(Integer.toString((rows * x) + y), 1, true);
+                if (mineList.contains((rows * x) + y)) isMine = true;
+                else isMine = false;
+                // create the mine buttons
+                MINE_GRID[x][y] = new MineButton(isMine, x, y);
                 butPan.add(MINE_GRID[x][y]);
                 MINE_GRID[x][y].setPreferredSize(new Dimension(25, 25));
 
@@ -281,6 +366,39 @@ class WinMine {
                         }
                     }
                 });
+            }
+        }
+
+        // find all the adjacent mine numbers
+        for (MineButton[] mineRow : MINE_GRID) {
+            for (MineButton thisMine : mineRow) {
+                // each minebutton:
+                int[] pos = thisMine.getPosition();
+                adjMines = 0;
+
+                // check the ring around the minebutton
+                // don't check positions below 0
+                int loRow = pos[0] - 1;
+                if (loRow < 0) loRow = 0;
+                int loCol = pos[1] - 1;
+                if (loCol < 0) loCol = 0;
+                // don't check positions above upper limit
+                int hiRow = pos[0] + 1;
+                if (hiRow >= rows) hiRow = rows - 1;
+                int hiCol = pos[1] + 1;
+                if (hiCol >= cols) hiCol = cols - 1;
+                // go around the ring
+                for (int i = loRow; i <= hiRow; i++) {
+                    for (int j = loCol; j <= hiCol; j++) {
+                        // increase adjMines if mine is present
+                        if (MINE_GRID[i][j].hasMine()) adjMines++;
+                        // don't have to exclude thisButton's mine because if
+                        // it's a mine, it won't display a number anyway. And
+                        // if it's not a mine, it won't affect the adjMines
+                        // count either.
+                    }
+                }
+                thisMine.setAdjMines(adjMines);
             }
         }
         return butPan;
